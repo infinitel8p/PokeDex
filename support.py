cb7ff8a23d797a699599925afa180433ddbbc6fa
infinitel8p <50703696+infinitel8p@users.noreply.github.com>
2021-03-07 12:29:44 +0100

    moved more stuff to support

--- support.py
+++ support.py
@@ -12,13 +12,36 @@ last_search = ""
 pokedex_version = "Pokedex for PokeOne v.3.1"
 
 #callback functions
-def Add_dummy():
-    #clears whole program
-    delete_item(pokedex_version, children_only = True)
-    add_text("You nuked the window. Please restart the applet", parent = pokedex_version)
-    with window(pokedex_version):
-        Build_menu()
-    add_dummy(width=150, height=150, parent = pokedex_version)
+def Build_app():
+    try:
+        delete_item(pokedex_version, children_only = True)
+    finally:
+        with window(pokedex_version, width = 520, height = 900):
+            set_window_pos(pokedex_version, 0, 0)
+            
+            #add menu to main window
+            Build_menu()
+
+            #image logo
+            add_drawing("logo", width=520, height=250) #create some space for the image
+            add_separator()
+
+            add_spacing(count = 5)
+            add_text("Search for Pokemon to get your results!", color = [200, 100, 100])
+            add_spacing(count = 2)
+
+            #Optional User Input
+            add_input_text("Input", width = 415, hint = "Insert Pokémon name here", on_enter = True, callback = Start_pokemon_check, label = "")
+            
+            #Button
+            add_spacing(count = 5)
+            add_button("Search", callback = Start_pokemon_check)
+            add_same_line()
+            add_button("Clear", callback = Clear_last_result)
+            add_spacing(count = 5)
+        
+        #place the image inside the space "logo"
+        draw_image("logo", r"PokeDex.png", [115,0], [365,250]) #padding 25
     
 def Browser():
     #opens chrome and navigates to project page
@@ -28,29 +51,45 @@ def Browser():
         #tries internet explorer if chrome is not available
         os.system("start iexplore https://github.com/infinitel8p/PokeDex/")
 
-def Build_menu(build_parent = pokedex_version):
+def Build_menu():
     #creates the menu bar
-    with menu_bar("Menu Bar", parent = build_parent):
-        with menu("Options"):
+    with menu_bar("Menu Bar", parent = pokedex_version):
+        with menu("Options", parent = "Menu Bar"):
             add_menu_item("Go to project page", callback = Browser, parent = "Options")
             add_menu_item("See the logs", callback = show_logger, parent = "Options")
             with menu("Not working yet", parent = "Options"):
-                add_menu_item("Nuke the Window", callback = Add_dummy, parent = "Not working yet")
+                add_menu_item("Nuke the Window", callback = Clear_app, parent = "Not working yet")
+                add_menu_item("Rebuild the Window", callback = Build_app, parent = "Not working yet")
+        add_menu_item("More Information", callback = Build_more_information, parent = "Menu Bar")
+        add_menu_item("Help", callback = Build_help, parent = "Menu Bar")
+
+def Build_help():
+    Clear_app()
+    add_child("help window", parent = pokedex_version, border = False)
+    add_text("This is the help window. We are still working on it", parent = "help window")
+
+def Build_more_information():
+    delete_item(pokedex_version, children_only = True)
+    Build_menu()
+    add_text("This is the more information window. We are still working on it", parent = pokedex_version)
+
+def Clear_app():
+    #clears whole program
+    delete_item(pokedex_version, children_only = True)
+    with window(pokedex_version):
+        Build_menu()
 
 def Clear_last_result():
     if search_runs == []:
         clear_drawing("logo")
         draw_image("logo", r"PokeDex.png", [115,0], [365,250])
     else:
-        Delete_last_result()
+        #clears last search result
+        clear_drawing("logo")
+        draw_image("logo", r"loading.png", [115,0], [365,250])
+        delete_item(search_runs[-1])
         clear_drawing("logo")
         draw_image("logo", r"PokeDex.png", [115,0], [365,250])
-
-def Delete_last_result():
-    #clears last search result
-    clear_drawing("logo")
-    draw_image("logo", r"loading.png", [115,0], [365,250])
-    delete_item(search_runs[-1])
 
 def Start_pokemon_check():
     #main function - handles pokemon search    
@@ -58,9 +97,6 @@ def Start_pokemon_check():
     input_value = input_value.lower()
     input_value = input_value.capitalize()
     
-    #global last_search
-    #if input_value != last_search:
-    last_search = input_value
     #prepaire for output
     if search_runs == []:
         #runs only with the first search (when search_runs[-1] does not exist)
@@ -87,6 +123,3 @@ def Start_pokemon_check():
         log_info(input_value)
         #log_debug("Debug Message")
         #log_warning("Warning Message")
-
-    #else:
-     #   log_error(f"recurring input detected: {input_value}")
