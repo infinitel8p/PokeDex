313ac719cd1305ecbeb4664f79d31b5369d4fdad
KrokoNinja <lenni.pfundtner@gmail.com>
2021-03-10 11:07:37 +0100

    update

--- uranium.py
+++ uranium.py
@@ -26,7 +26,7 @@ class Pokemon ():
             return self.pokemon_type
         else:
             return str(self.pokemon_type, self.pokemon_type2)
-    
+
     def get_versionen(self):
         if self.pokemon_versionen == None:
             pass
@@ -42,6 +42,18 @@ Eletux = Pokemon("Eletux", "#005", "google.de/bild", "google.de/bild2", "Water",
 Electruxo = Pokemon("Electruxo", "#006", "google.de/bild", "google.de/bild2", "Water", "Electric")
 Nuclear_Chyinmunk = Pokemon("Nuclear Chyinmunk", "#007", "google.de/bild", "google.de/bild2", "Normal", "Nuclear")
 Chyinmunk = Pokemon("Chyinmunk", "#007", "google.de/bild", "google.de/bild2", "Normal", None, Nuclear_Chyinmunk)
+Kinetmunk = Pokemon("Kinetmunk", "#008", "google.de/bild", "google.de/bild2", 2, "Normal", "Electric")   #Nuclear
+Birbie = Pokemon("Birbie", "#009", "google.de/bild", "google.de/bild2", 1, "Normal", "Flying")
+Aveden = Pokemon("Aveden", "#010", "google.de/bild", "google.de/bild2", 1, "Normal", "Flying")
+Splendifowl = Pokemon("Splendifowl", "#011", "google.de/bild", "google.de/bild2", 1, "Normal", "Flying")
+Cubbug = Pokemon("Cubbug", "#012", "google.de/bild", "google.de/bild2", 1, "Bug")
+Cubblfly = Pokemon("Cubblfly", "#013", "google.de/bild", "google.de/bild2", 1, "Bug", "Fairy")
+Nimflora = Pokemon("Nimflora", "#014", "google.de/bild", "google.de/bild2", 1, "Bug", "Fairy")
+Barewl = Pokemon("Barewl", "#015", "google.de/bild", "google.de/bild2", 1, "Steel", "Rock")
+Dearewl = Pokemon("Dearewl", "#016", "google.de/bild", "google.de/bild2", 1, "Steel", "Rock")
+Gararewl = Pokemon("Gararewl", "#017", "google.de/bild", "google.de/bild2", 1, "Steel")
+Grozard = Pokemon("Grozard", "#018", "google.de/bild", "google.de/bild2", 1, "Ground")
+Terlard = Pokemon("Terlard", "#019", "google.de/bild", "google.de/bild2", 1, "Ground", "Dragon")
 
 Pokédex_Uranium = [Orchynx, Metalynx, Raptorch, Archilles, Eletux, Electruxo, Chyinmunk, Nuclear_Chyinmunk]
 
