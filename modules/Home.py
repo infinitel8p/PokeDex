0a42bfff8068f5c77a46b25729b15373cbaa7c1b
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-02-11 16:59:49 +0100

    fixed rowspan design

--- modules/Home.py
+++ modules/Home.py
@@ -74,7 +74,7 @@ class Home(customtkinter.CTkFrame):
             self.search_bar.get())
 
         # remove old widgets
-        needs_to_go = ["separator_1", "image_label",
+        needs_to_go = ["separator_1", "separator_2", "image_label",
                        "search_label", "search_bar", "button_frame"]
         for widget in needs_to_go:
             self.__dict__[widget].destroy()
@@ -96,8 +96,8 @@ class Home(customtkinter.CTkFrame):
         self.image = customtkinter.CTkImage(
             self.default, size=(245, 245))
         self.image_label = customtkinter.CTkLabel(
-            self.search_frame, image=self.image, text="")
-        self.image_label.grid(row=0, column=2, rowspan=10,
+            self.search_frame, image=self.image, text="", anchor="ne")
+        self.image_label.grid(row=0, column=2, rowspan=4,
                               padx=(5, 0), sticky="e")
 
         # add return button
@@ -110,30 +110,32 @@ class Home(customtkinter.CTkFrame):
         self.results_pokemon_name = customtkinter.CTkLabel(
             self.search_frame, text="", width=180, wraplength=180, justify="left", anchor="nw")
         self.results_pokemon_name.grid(
-            row=1, column=0, sticky="nw")
+            row=1, column=0, sticky="w")
 
         # add index nr
         self.results_pokemon_index = customtkinter.CTkLabel(
             self.search_frame, text="", width=50, justify="right", anchor="ne")
         self.results_pokemon_index.grid(
-            row=1, column=1, padx=(0, 10), sticky="ne")
+            row=1, column=1, padx=(0, 10), sticky="e")
 
         # add types
-        self.results_frame = customtkinter.CTkFrame(
+        # todo: width of the buttons should be dynamic (based on type)
+        width = 250/2
+        self.types_frame = customtkinter.CTkFrame(
             self.search_frame, width=250, fg_color="transparent")
-        self.results_frame.grid(row=2, column=0, columnspan=2, pady=5)
+        self.types_frame.grid(row=2, column=0, columnspan=2)
 
         self.results_pokemon_type_1 = customtkinter.CTkButton(
-            self.results_frame, text="", fg_color="transparent", border_color="white", text_color_disabled="white",
-            border_width=1, width=105, height=20, corner_radius=45, state="disabled")
+            self.types_frame, text="", fg_color="transparent", border_color="white", text_color_disabled="white",
+            border_width=1, width=width, height=20, corner_radius=45, state="disabled")
         self.results_pokemon_type_1.grid(
-            row=1, column=0, sticky="w", padx=(0, 10))
+            row=1, column=0, sticky="w", padx=(0, 5))
 
         self.results_pokemon_type_2 = customtkinter.CTkButton(
-            self.results_frame, text="", fg_color="transparent", border_color="white", text_color_disabled="white",
-            border_width=1, width=105, height=20, corner_radius=45, state="disabled")
+            self.types_frame, text="", fg_color="transparent", border_color="white", text_color_disabled="white",
+            border_width=1, width=width, height=20, corner_radius=45, state="disabled")
         self.results_pokemon_type_2.grid(
-            row=1, column=1, sticky="e", padx=(10, 0))
+            row=1, column=1, sticky="e", padx=(5, 0))
 
         # add evolutions
         # topframe
@@ -144,13 +146,13 @@ class Home(customtkinter.CTkFrame):
         # evolution 1
         self.evoltion_1_frame = customtkinter.CTkFrame(self.evolutions_frame, fg_color="transparent", border_color="white",
                                                        border_width=1, width=75, corner_radius=10)
-        self.evoltion_1_frame.grid(row=0, column=0, rowspan=8)
+        self.evoltion_1_frame.grid(row=0, column=0)
 
         self.sprite = customtkinter.CTkImage(Image.open(
             r"C:\Users\Ludo\Desktop\PokeDex\images\download\front_default_1.png"), size=(50, 50))
         self.image_label = customtkinter.CTkLabel(
             self.evoltion_1_frame, image=self.sprite, text="")
-        self.image_label.grid(row=0, pady=(1, 0))
+        self.image_label.grid(row=0, pady=(2, 0))
 
         self.evoltion_1_index = customtkinter.CTkLabel(
             self.evoltion_1_frame, text="#001", font=("Pixel Operator 8", 7), height=20)
@@ -167,14 +169,13 @@ class Home(customtkinter.CTkFrame):
         # evolution 2
         self.evoltion_2_frame = customtkinter.CTkFrame(self.evolutions_frame, fg_color="transparent", border_color="white",
                                                        border_width=1, width=75, corner_radius=10)
-        self.evoltion_2_frame.grid(
-            row=0, column=1, padx=10, rowspan=8)
+        self.evoltion_2_frame.grid(row=0, column=1, padx=10)
 
         self.sprite = customtkinter.CTkImage(Image.open(
             r"C:\Users\Ludo\Desktop\PokeDex\images\download\front_default_2.png"), size=(50, 50))
         self.image_label = customtkinter.CTkLabel(
             self.evoltion_2_frame, image=self.sprite, text="")
-        self.image_label.grid(row=0, pady=(1, 0))
+        self.image_label.grid(row=0, pady=(2, 0))
 
         self.evoltion_2_index = customtkinter.CTkLabel(
             self.evoltion_2_frame, text="#002", font=("Pixel Operator 8", 7), height=20)
@@ -191,13 +192,13 @@ class Home(customtkinter.CTkFrame):
         # evolution 3
         self.evoltion_3_frame = customtkinter.CTkFrame(self.evolutions_frame, fg_color="transparent", border_color="white",
                                                        border_width=1, width=75, corner_radius=10)
-        self.evoltion_3_frame.grid(row=0, column=2, rowspan=8)
+        self.evoltion_3_frame.grid(row=0, column=2)
 
         self.sprite = customtkinter.CTkImage(Image.open(
             r"C:\Users\Ludo\Desktop\PokeDex\images\download\front_default_3.png"), size=(50, 50))
         self.image_label = customtkinter.CTkLabel(
             self.evoltion_3_frame, image=self.sprite, text="")
-        self.image_label.grid(row=0, pady=(1, 0))
+        self.image_label.grid(row=0, pady=(2, 0))
 
         self.evoltion_3_index = customtkinter.CTkLabel(
             self.evoltion_3_frame, text="#003", font=("Pixel Operator 8", 7), height=20)
@@ -219,11 +220,11 @@ class Home(customtkinter.CTkFrame):
         # add label for results
         self.results = customtkinter.CTkLabel(
             self.vertical_scrolled_frame, text="", anchor="w", justify="left", width=450, wraplength=450)
-        self.results.pack(pady=10)
+        self.results.pack()
 
         # add results to label
         self.results_pokemon_name.configure(
-            text=pokemon_name)
+            text="Name: Charizard\nHabitat: Mountain")
         self.results_pokemon_index.configure(text="#006")
         self.results_pokemon_type_1.configure(text="Fire")
         self.results_pokemon_type_2.configure(text="Flying")
